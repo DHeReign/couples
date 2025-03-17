31559e109d0fa8f22fba8a1d988377720a802e4f
DHeReign <varnik@icloud.com>
2025-03-17 20:06:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const button = document.querySelector(".board__button");
+const board = document.querySelector(".board");
+
+function createBoard() {
+    board.value = "";
+};
+
+addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    const input = document.querySelector(".board__input");
+    let inputValue = input.value;
+    let cards;
+
+    if(inputValue >= 2 && inputValue <= 6 && inputValue % 2 != 0) {
+        cards = inputValue * inputValue;
+    }
+    else {
+        cards = 4;
+    }
+
+    createBoard();
+});
